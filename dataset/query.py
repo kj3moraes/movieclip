50a8f8028bd89dad8cee1df12090b70c40bbdcfd
Keane Moraes <lordvader3002@gmail.com>
2023-12-19 14:01:04 +0400

    fix(query): bug fixes

--- dataset/query.py
+++ dataset/query.py
@@ -1,5 +1,5 @@
 import os
-import requests 
+import requests
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -7,15 +7,16 @@ load_dotenv()
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
 BASE_URL = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&"
 
+
 def __process_response(response: dict) -> dict:
     """
     Function takes a response from OMDb and converts into a dictionary that we want
     """
-    
-    response['Director'] = response.get('Director', '').replace(", ", ",").split(",") 
-    response['Writer'] = response.get('Writer', '').replace(", ", ",").split(",")
-    response['Actors'] = response.get('Actors', '').replace(", ", ",").split(",")
-    response['Genre'] = response.get('Genre', '').replace(", ", ",").split(",")
+
+    response["Director"] = response.get("Director", "").replace(", ", ",").split(",")
+    response["Writer"] = response.get("Writer", "").replace(", ", ",").split(",")
+    response["Actors"] = response.get("Actors", "").replace(", ", ",").split(",")
+    response["Genre"] = response.get("Genre", "").replace(", ", ",").split(",")
     return response
 
 
@@ -33,19 +34,23 @@ def get_movie_poster_from_id(id: str):
 
     r = requests.get(url)
     if r.status_code != requests.codes.ok:
-        print(f"Failed to get data for {title}")
+        print(f"Failed to get data for {id}")
     return __process_response(r.json())
 
 
-CAPTIONING_API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+CAPTIONING_API_URL = (
+    "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+)
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
+
 def get_image_caption(im_path: Path):
-    with open(filename, "rb") as f:
+    with open(im_path, "rb") as f:
         data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
+    response = requests.post(CAPTIONING_API_URL, headers=headers, data=data)
     return response.json()
+
 
 # output = get_image_caption("cats.jpg")
 print(get_movie_data_from_title("The Laundromat"))
