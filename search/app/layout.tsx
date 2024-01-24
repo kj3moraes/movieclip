1df50c5e09295e03bad9346dc6f3206cf6f44294
Keane Moraes <lordvader3002@gmail.com>
2024-01-23 20:50:54 -0500

    feat: initial commit for the fast api server

--- search/app/layout.tsx
+++ search/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Providers } from './providers'
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
---
+++ search/backend/image_search.py
@@ -0,0 +1,5 @@
+from qdrant_client import QdrantClient
+from typing import List
+
+def search_images(text: str) -> List[str]:
+    pass
