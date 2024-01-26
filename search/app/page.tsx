0fab342957a45c2ae421a82864ef23e93ebef89c
Keane Moraes <lordvader3002@gmail.com>
2024-01-26 15:08:31 -0500

    feat: added placeholder accordions

--- search/app/page.tsx
+++ search/app/page.tsx
@@ -2,6 +2,14 @@
 
 import { useState } from 'react';
 import { Input } from '@chakra-ui/react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box
+} from '@chakra-ui/react'
 
 import Image from 'next/image'; // Import if you want to use Next.js' Image component
 
@@ -19,6 +27,60 @@ export default function Home() {
       </div>
 
       <Input className="w-full max-w-screen-sm p-2 border rounded" placeholder="Search for images" /> 
+      <div className="w-full max-w-screen-sm p-2" >
+        <Accordion allowToggle className="my-3">
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box textAlign='left'>
+                  Tip #1 : Type in the description of the scene for best results
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box textAlign='left'>
+                  Tip #2 : You can search by director name
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box textAlign='left'>
+                  Tip #3 : You can search by movie name
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
