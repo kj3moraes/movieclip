69a35ebd2a6fc8f78fb11d14fc677d165d59e6ef
Keane Moraes <lordvader3002@gmail.com>
2024-01-27 12:19:43 -0500

    fix: image size fix

--- model/example.py
+++ model/example.py
@@ -12,12 +12,17 @@ inputs = processor(
     text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True
 )
 
-print(inputs.keys())
-print(inputs["pixel_values"].shape)  # torch.Size([2, 3, 224, 224]
-print(inputs["input_ids"].shape)  # torch.Size([2, 8])
-print(inputs["input_ids"])  # torch.Size([2, 8]
-print(inputs["attention_mask"].shape)  # torch.Size([2, 8])
-print(inputs["attention_mask"])  # torch.Size([2, 8]
+# print(inputs.keys())
+# print(inputs["pixel_values"].shape)  # torch.Size([2, 3, 224, 224]
+# print(inputs["input_ids"].shape)  # torch.Size([2, 8])
+# print(inputs["input_ids"])  # torch.Size([2, 8]
+# print(inputs["attention_mask"].shape)  # torch.Size([2, 8])
+# print(inputs["attention_mask"])  # torch.Size([2, 8]
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
 probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+ds_with_embeddings = ds_with_embeddings.map(lambda example:
+                                          {'image_embeddings': model.get_image_features(
+                                              **processor([example["image"]], return_tensors="pt")
+                                              .to("cuda"))[0].detach().cpu().numpy()})
+print(probs)
