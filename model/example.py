db3f0c1979f1439670da12f42a8c03a2bc1085db
Keane Moraes <lordvader3002@gmail.com>
2024-01-27 10:54:13 -0500

    fix: added labels and clip tokenization

---
+++ model/example.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import requests
+from transformers import AutoProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = processor(
+    text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True
+)
+
+print(inputs.keys())
+print(inputs["pixel_values"].shape)  # torch.Size([2, 3, 224, 224]
+print(inputs["input_ids"].shape)  # torch.Size([2, 8])
+print(inputs["input_ids"])  # torch.Size([2, 8]
+print(inputs["attention_mask"].shape)  # torch.Size([2, 8])
+print(inputs["attention_mask"])  # torch.Size([2, 8]
+outputs = model(**inputs)
+logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
